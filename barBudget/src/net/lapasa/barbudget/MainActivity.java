7bbe8e3b82eab809140619eb437c4699b4043084
marklapasa <mark.lapasa@gmail.com>
2014-05-30 07:47:44 -0400

    Fixed various bugs

--- barBudget/src/net/lapasa/barbudget/MainActivity.java
+++ barBudget/src/net/lapasa/barbudget/MainActivity.java
@@ -1,12 +1,14 @@
 package net.lapasa.barbudget;
 
 import net.lapasa.barbudget.fragments.CategoryListFragment;
+import net.lapasa.barbudget.models.PeriodModel;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentManager.OnBackStackChangedListener;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import de.keyboardsurfer.android.widget.crouton.Crouton;
 
 public class MainActivity extends Activity implements OnBackStackChangedListener
@@ -15,6 +17,8 @@ public class MainActivity extends Activity implements OnBackStackChangedListener
     private static final String TAG = MainActivity.class.getName();
 	private FragmentManager fm;
 
+	private ViewPager viewPager = null;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -23,10 +27,12 @@ public class MainActivity extends Activity implements OnBackStackChangedListener
         fm.addOnBackStackChangedListener(this);
         ((BarBudgetApplication) getApplication()).inject(this);;
         setContentView(R.layout.activity_main);
+//        viewPager = (ViewPager) findViewById(R.id.root)
         
         
         /* If there are no categories, display the default fragment, i.e. fragment_no_categories */
         showFragment(new CategoryListFragment());
+//        showFragment(CategoryListFragment.create(PeriodModel.RANGE_TODAY));
     }
 
 
@@ -65,3 +71,4 @@ public class MainActivity extends Activity implements OnBackStackChangedListener
 		}
 	}
 }
+
